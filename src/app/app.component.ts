da797c5d23b1e820a6b7b1994037ced0776efc06
jejejekeie <evap040@gmail.com>
2023-11-28 16:29:41 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Renderer2, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -11,4 +11,25 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'devportfolio';
+  lastScrollTop: number = 0;
+
+    constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+        const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+        const navbar = this.el.nativeElement.querySelector('nav');
+        const navLine = this.el.nativeElement.querySelector('.nav-line');
+
+        if (currentScroll > this.lastScrollTop) {
+            this.renderer.setStyle(navbar, 'top', '-60px'); 
+            this.renderer.addClass(navLine, 'full-width-line'); 
+        } else {
+            this.renderer.setStyle(navbar, 'top', '0');
+            if (currentScroll === 0) {
+                this.renderer.removeClass(navLine, 'full-width-line');
+            }
+        }
+        this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+    }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+    providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
+  })
+  export class AppRoutingModule { }
